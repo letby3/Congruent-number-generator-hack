0b7949c478ed3fc6fd54af5d4501a795e200f591
letby3 <fvahit2@gmail.com>
2022-12-07 04:10:16 +0300

    v0.91

--- LinearCongruentMethod.cpp
+++ LinearCongruentMethod.cpp
@@ -12,7 +12,9 @@ using namespace std;
 
 int main(){
 	for (int i = 0; i < 10000; i++) {
-		int answ = start_hack(1, 0);
+		int answ = start_hack(1, 0); 
+		/*Show_ans (first Parameter) = 1/0 показывать ответы или нет
+		AutoR = 1/0, авторежим генерации(автоматически выбирает 3)*/
 		cout << answ << endl;
 		if (answ == 0)
 			system("PAUSE");
